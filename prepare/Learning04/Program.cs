2831fa4d61575241182a7132375f69ea2fb0744b
eluwa101 <eluwamonday2017@gmail.com>
2024-02-05 03:29:25 +0100

    added Assignment activity program

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment week4 = new Assignment("kate Mandy", "Indices");
+        Console.WriteLine(week4.GetSummary());
+
+        MathAssignment indices = new MathAssignment("Coco Charles", "Algebra", "1.1", "9-17");
+        Console.WriteLine(indices.GetSummary());
+        Console.WriteLine(indices.GetHomeworkList());
+
+        WritingAssignment essay = new WritingAssignment("Olu Smith", "Pan History", "The Golden Stool, our heritage");
+        Console.WriteLine(essay.GetSummary());
+        Console.WriteLine(essay.GetWritingInformation());
     }
 }
